a5106fb49f1c3a34f5575b3339fb8a5d5635889c
Vincent Danion Ong <vincent98ong@kingmakergames.co>
2021-02-09 21:16:30 +0800

    v0.0.1
    v0.0.1
    First test git

---
+++ Alone, Together pt2/Assets/Cloner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject box;
+    [SerializeField]
+    private int spawnVal;
+    [SerializeField]
+    private float distanceVal; // more than 20 will seem obvious
+    [SerializeField]
+    private List<GameObject> clone = new List<GameObject>();
+    Vector3 endPos;
+
+    private void Start()
+    {
+        endPos = new Vector3 (0,0,0);
+        for (int i = 0; i < spawnVal; i++)
+        {
+            clone.Add(box);
+        }
+
+        foreach (GameObject clones in clone)
+        {
+            Instantiate(clones, endPos, Quaternion.identity);
+            endPos.x = endPos.x + distanceVal;
+        }
+    }
+
+}
