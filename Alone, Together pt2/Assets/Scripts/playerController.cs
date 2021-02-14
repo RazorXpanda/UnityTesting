ef14e0b5b4849e8dedf7a1a2ff09b87cb0328ef7
DutchDave <46603781+Drestdave@users.noreply.github.com>
2021-02-14 13:50:34 +0100

    v0.0.5
    Basic functionality movement wise

---
+++ Alone, Together pt2/Assets/Scripts/playerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform shootingPoint;
+    [SerializeField] private GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the rigidbody attached to the player
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbPlayer.velocity = -transform.forward * moveSpeed;
+        }
+        //Move backwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            rbPlayer.velocity = transform.forward * moveSpeed;
+        }
+        //Move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            rbPlayer.velocity = -transform.right * moveSpeed;
+        }
+        //Move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            rbPlayer.velocity = transform.right * moveSpeed;
+        }
+
+
+        //Shoot
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Shoot!");
+        }
+
+        rotatePlayer();
+    }
+
+    //Player Rotation
+    void rotatePlayer()
+    {
+
+    }
+}
