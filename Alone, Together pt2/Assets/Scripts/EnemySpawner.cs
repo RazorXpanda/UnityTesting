e79316816c3759ea44cb194a0490c717a69eee2c
DutchDave <46603781+Drestdave@users.noreply.github.com>
2021-02-14 20:11:46 +0100

    v0.2.1
    - Set Gravity Scale on RB2D of the enemy to 0
    - Implemented enemy movement towards the player position. Included options to vary the distance at which the enemy will halt from the player.
    - Implemented the enemy looking towards the player position
    - Implemented a maximum enemy cap in the enemy spawner script, can be adjusted.
    - Decreased the enemy spawn rate
    -  Added look direction indicator (sprite) on the enemy

--- Alone, Together pt2/Assets/Scripts/EnemySpawner.cs
+++ Alone, Together pt2/Assets/Scripts/EnemySpawner.cs
@@ -11,16 +11,19 @@ public class EnemySpawner : MonoBehaviour
     public GameObject botSpawn;
     [SerializeField]
     private GameObject enemy; //enemy prefab
+    [Range (1, 100)] public int maxEnemies = 30;
+    public int currentEnemies;
     [SerializeField][Tooltip("This value CANNOT be 0, I inversed it")]
     private float spawnRate; //The higher the value, the faster the spawn rate
     
     private void Start()
     {
         StartCoroutine(Spawner(1/spawnRate));
+        currentEnemies = 0;
     }
     IEnumerator Spawner(float spawnRate)
     { 
-        while (true)
+        while (currentEnemies < maxEnemies)
         {
             //Get a new randomized range of values each spawn
             float topSpawnPos = topSpawn.transform.position.x + Random.Range(-12f,12f);
@@ -34,25 +37,29 @@ public class EnemySpawner : MonoBehaviour
             {
                 case 0: //Spawns using the top spawnpoint
                 Instantiate(enemy, new Vector2(topSpawnPos, topSpawn.transform.position.y), Quaternion.identity);
-                yield return new WaitForSeconds(spawnRate);
+                    currentEnemies++;
+                    yield return new WaitForSeconds(spawnRate);
                 break;
 
                 case 1: //Spawns using the bottom spawnpoint
                 Instantiate(enemy, new Vector2(botSpawnPos, botSpawn.transform.position.y), Quaternion.identity);
-                yield return new WaitForSeconds(spawnRate);
+                    currentEnemies++;
+                    yield return new WaitForSeconds(spawnRate);
                 break;
 
                 case 2: //Spawns using the right spawnpoint
                 Instantiate(enemy, new Vector2(rightSpawn.transform.position.x, rightSpawnPos), Quaternion.identity);
-                yield return new WaitForSeconds(spawnRate);
+                    currentEnemies++;
+                    yield return new WaitForSeconds(spawnRate);
                 break;
 
                 case 3: //Spawns using the left spawnpoint
                 Instantiate(enemy, new Vector2(leftSpawn.transform.position.x, leftSpawnPos), Quaternion.identity);
-                yield return new WaitForSeconds(spawnRate);
+                    currentEnemies++;
+                    yield return new WaitForSeconds(spawnRate);
                 break;
             }
-            ;
+            //; What is this for? ~Dave
 
             
         }
---
+++ Alone, Together pt2/Assets/Scripts/enemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D enemyRB;
+
+    public float approachLimit; //closest it wants to go to the player
+    public float approachMin; //farthest away it will allow itself to halt
+    private float approachRange;
+    [SerializeField] [Range(0, 3)] float moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        approachRange = Random.Range(approachLimit, approachMin);
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        //Move towards the player
+        Vector3 moveDirection = playerPos - transform.position;
+        if(moveDirection.magnitude > approachRange)
+        {
+            Vector2 moveDir = new Vector2(moveDirection.x, moveDirection.y) * 0.5f;
+            enemyRB.MovePosition(enemyRB.position + moveDir * moveSpeed * Time.fixedDeltaTime);
+        }
+        
+
+
+        //Look towards the player
+        Vector2 lookDir = player.transform.position - transform.position;
+        float angle = Vector2.Angle(Vector2.up, lookDir);
+
+        if(lookDir.x >= 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -angle);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+}
