877a25b3967bc1c5be2c6f3a8f344a663ce692b5
Vincent Danion Ong <vincent98ong@kingmakergames.co>
2021-02-14 22:00:09 +0800

    v0.1
    - Added rotation script
    - Disabled movement by velocity codeblock, implemented rigidbody's movement by transform.position instead
    - Change gameObj from 3D to 2D (Sprite)
    - Changed rb body type to kinematic
    - Movement of sprite is now linear and straight

--- Alone, Together pt2/Assets/Scripts/playerController.cs
+++ Alone, Together pt2/Assets/Scripts/playerController.cs
@@ -4,43 +4,86 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    private Rigidbody rbPlayer;
+    private Rigidbody2D rbPlayer;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform shootingPoint;
     [SerializeField] private GameObject bullet;
+    [SerializeField] private Camera cam;
+    private Vector2 movement;
+    private float angle;
 
     // Start is called before the first frame update
     void Start()
     {
         //Get the rigidbody attached to the player
-        rbPlayer = GetComponent<Rigidbody>();
+        //Temporarily disable due to rigidbody issues
+        rbPlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        movement.x = Input.GetAxisRaw("Horizontal"); //PC input
+        movement.y = Input.GetAxisRaw("Vertical");// PC input
+        Vector2 direction = Vector2.up * movement.y + Vector2.right * movement.x;
+        rbPlayer.MovePosition(rbPlayer.position + direction * moveSpeed * Time.fixedDeltaTime);
 
-        //Move forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            rbPlayer.velocity = -transform.forward * moveSpeed;
-        }
-        //Move backwards
-        if (Input.GetKey(KeyCode.S))
-        {
-            rbPlayer.velocity = transform.forward * moveSpeed;
-        }
-        //Move left
-        if (Input.GetKey(KeyCode.A))
-        {
-            rbPlayer.velocity = -transform.right * moveSpeed;
-        }
-        //Move right
-        if (Input.GetKey(KeyCode.D))
-        {
-            rbPlayer.velocity = transform.right * moveSpeed;
-        }
+        var mousePos = Input.mousePosition;
+        var screenPos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - cam.transform.position.z));
+     
+        rbPlayer.transform.eulerAngles = new Vector3(0,0,Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x))* Mathf.Rad2Deg);;
+
+
+        
+        // if (movement.x != 0 && movement.y != 0)
+        // {
+        //     angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        //     rbPlayer.rotation = angle;
+        // }
+
+        // if (movement != Vector2.zero)
+        // {
+        //     rbPlayer.MoveRotation(angle);
+        // }
+
+        // //I reckon we should not use velocity. Players cannot control the speed of the ball after key release.
+        // //Move forward
+        // if (Input.GetKey(KeyCode.W))
+        // {
+        //     //rbPlayer.velocity = -transform.forward * moveSpeed;
+        //     Vector3 pos = transform.position;
+        //     pos += transform.up * moveSpeed * Time.deltaTime;
+        //     transform.position = pos;
+
+        // }
+        // //Move backwards
+        // if (Input.GetKey(KeyCode.S))
+        // {
+        //     rbPlayer.velocity = transform.forward * moveSpeed;
+        //     Vector3 pos = transform.position;
+        //     pos += -transform.up * moveSpeed * Time.deltaTime;
+        //     transform.position = pos;
+
+        // }
+        // //Move left
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     //rbPlayer.velocity = -transform.right * moveSpeed;
+        //     Vector3 pos = transform.position;
+        //     pos += -transform.right * moveSpeed * Time.deltaTime;
+        //     transform.position = pos;
+
+        // }
+        // //Move right
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     //rbPlayer.velocity = transform.right * moveSpeed;
+        //     Vector3 pos = transform.position;
+        //     pos += transform.right * moveSpeed * Time.deltaTime;
+        //     transform.position = pos;
+
+        // }
 
 
         //Shoot
@@ -49,12 +92,6 @@ public class playerController : MonoBehaviour
             Debug.Log("Shoot!");
         }
 
-        rotatePlayer();
-    }
-
-    //Player Rotation
-    void rotatePlayer()
-    {
-
     }
 }
+
