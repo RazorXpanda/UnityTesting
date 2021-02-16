4492f0c7a143754a804921cd78c8f2eb30b9a44b
Vincent Danion Ong <vincent98ong@kingmakergames.co>
2021-02-16 21:07:05 +0800

    v0.2.6.2
    - Added melee combat
    - Added melee swing anim

---
+++ Alone, Together pt2/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    private Transform meleeTransform;
+    public float attackRange = 2f;
+    public LayerMask enemyLayers;
+    public int meleeDamage = 30;
+
+    void Start()
+    {
+        meleeTransform = this.transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(meleeTransform.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            var data = enemy.gameObject.GetComponent<EntityData>();
+            GameEvents.current.DamageReceived(data.GetInstanceID(), meleeDamage);
+        }
+    }
+}
