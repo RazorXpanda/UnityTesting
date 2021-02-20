294ce769b887c92d9895601a397c4af43da13a7b
Vincent Danion Ong <vincent98ong@kingmakergames.co>
2021-02-20 16:25:54 +0800

    v0.5.0
    New credits section!
    New win panel and lose panel
    added tips randomizer

--- Alone, Together pt2/Assets/Scripts/GameEvents.cs
+++ Alone, Together pt2/Assets/Scripts/GameEvents.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameEvents : MonoBehaviour
 {
     public static GameEvents current;      // public singletone of the event system
+    public GameObject losePanel;    //Activates lose Panel
+    public GameObject winPanel; //Actives Win Panel
+    public TipsList tipsList;
 
     private void Awake()
     {
@@ -32,19 +36,59 @@ public class GameEvents : MonoBehaviour
     public void ObjectiveLost()
     {
         Debug.Log("Start lost end screen here");
-        StartCoroutine(EndScreen());
+        StartCoroutine(EndScreen(true, false));
     }
 
     public void ObjectiveAchieved()
     {
         Debug.Log("Start win end screen here");
-        StartCoroutine(EndScreen());
+        StartCoroutine(EndScreen(true, true));
     }
 
-    private IEnumerator EndScreen()
+    bool isFaded = false;
+    private IEnumerator EndScreen(bool fadeIn, bool isWinPanel)
     {
+        
         Debug.Log("Starting end screen");
         // Shows the end screen here
+
+        if(isWinPanel)
+        {
+        winPanel.SetActive(true);       
+        Text[] fadeText = winPanel.GetComponentsInChildren<Text>();
+        
+        if (fadeIn)
+        {   
+            foreach(Text item in fadeText)
+            {
+                for (float i = 0; i <= 5; i += Time.deltaTime) //1 second fade
+                {
+                    item.color = new Color(1,1,1,i);
+                    yield return null;
+                }
+            }
+        }  
+        }
+
+        else if (!isWinPanel)
+        {
+            losePanel.SetActive(true);       
+            Text[] fadeText = losePanel.GetComponentsInChildren<Text>();
+            tipsList.Tips();
+            
+            if (fadeIn)
+            {   
+                foreach(Text item in fadeText)
+                {
+                    for (float i = 0; i <= 5; i += Time.deltaTime) //1 second fade
+                    {
+                        item.color = new Color(1,1,1,i);
+                        yield return null;
+                    }
+                }
+            }  
+        }
+                      
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene("MainMenu");
     }
---
+++ Alone, Together pt2/Assets/Scripts/TipsList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipsList : MonoBehaviour
+{
+    public Text tipsText;
+    private List<string> listOfTips = new List<string>();
+    private void Start()
+    {
+        listOfTips.Add("Don't forget to defend your castle!");
+        listOfTips.Add("Light up all the torches to save the world!");
+        listOfTips.Add("Some ghosts may drop spirits.");
+        listOfTips.Add("There are items hidden around the vases across the map!");
+        listOfTips.Add("Ghosts will aim for the castle. Defend it!");
+        listOfTips.Add("Ghosts can see much better in the dark.");
+    }
+
+    public void Tips()
+    {
+        string randomListString = listOfTips[Random.Range(0, listOfTips.Count - 1)];
+        tipsText.text = "Tip: " + randomListString;
+    }
+}
