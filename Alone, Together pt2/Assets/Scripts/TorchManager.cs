6853eb14c49a8eef2a36761f7cacd54a9503ec9d
DutchDave <46603781+Drestdave@users.noreply.github.com>
2021-02-20 22:47:02 +0100

    v0.5.4
    - Added torch counter

--- Alone, Together pt2/Assets/Scripts/TorchManager.cs
+++ Alone, Together pt2/Assets/Scripts/TorchManager.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TorchManager : MonoBehaviour
 {
     List<GameObject> torches;
     bool sendWinSignal;
+    int totalTorches;
+    public int currentTorches;
+    [SerializeField] GameObject torchUI;
+    Text torchText;
+
 
     private void Start()
     {
+        
         sendWinSignal = false;
         if (torches == null)
         {
@@ -18,6 +25,10 @@ public class TorchManager : MonoBehaviour
                 torches.Add(torch);
             }
         }
+        currentTorches = 0;
+        totalTorches = torches.Count;
+        torchText = torchUI.GetComponent<Text>();
+        torchText.text = currentTorches + "/ 22";
     }
 
     private void Update()
@@ -27,6 +38,8 @@ public class TorchManager : MonoBehaviour
             GameEvents.current.ObjectiveAchieved();
             sendWinSignal = true;
         }
+
+        torchText.text = currentTorches + "/ 22";
     }
 
     bool IsAllTorchesLit()
@@ -39,4 +52,5 @@ public class TorchManager : MonoBehaviour
 
         return true;
     }
+
 }
